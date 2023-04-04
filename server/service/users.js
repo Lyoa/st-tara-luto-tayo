68ce7b8c8146baba050acfbdd97ffbf50b982eab
Shaira de Leon <sczaydeleon@gmail.com>
2023-04-05 05:25:24 +0800

    fix: updated connection of database to my capstone file

--- server/service/users.js
+++ server/service/users.js
@@ -4,15 +4,6 @@ class UserService {
     async getUsers() {
         return await userRepository.getUsers()
     }
-
-    // CREATE
-    // async createUser() {}
-
-    // UPDATE
-    // async updateUser() {}
-
-    // DELETE
-    // async deleteUser()
 }
 
 module.exports = new UserService()
